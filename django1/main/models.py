72ec697958379dd9fc10c7037bc73602caa2816e
valentin g <33gva@mail.ru>
2021-01-08 21:43:35 +0300

    add patient's FK and create_at fields, delete Normal_values table

--- django1/main/models.py
+++ django1/main/models.py
@@ -1,35 +1,39 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
-class Patient(models.Model):
+
+class AutoDateTimeField(models.DateTimeField):
+    def pre_save(self, model_instance, add):
+        return timezone.now()
+
+    created_at = models.DateField(default=timezone.now)
+    updated_at = models.DateTimeField(default=timezone.now)
+
+class Patient(models.Model, AutoDateTimeField):
     """Пациент"""
     surname = models.CharField('Фамилия', max_length=50)
     name = models.CharField('Имя', max_length=50)
     patronymic = models.CharField('Отчество', max_length=50)
 
+    created_at = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         return self.surname
 
-class Result(models.Model):
+class Result(models.Model, AutoDateTimeField):
     """"Результаты"""
-    patient = models.ForeignKey(Patient, on_delete=models.CASCADE)
+    patient = models.ForeignKey(Patient, on_delete=models.CASCADE, default=None)
+
     blast_cell = models.FloatField('Количество бластных клеток')
     promyelocytes = models.FloatField('Количество милеоцитов')
     neutrophils_myelocytes = models.FloatField('Количество нейтрофилов')
     neutrphils_metamyelocytes = models.FloatField('Нейтрофилы: Миелоциты')
 
+    created_at = models.DateTimeField(default=timezone.now)
+
     def __str__(self):
         return str(self.blast_cell)
-
-class Normal_value(models.Model):
-    """"Нормальные значения"""
-    blast_cell = models.FloatField('Количество бластных клеток', default=0.5)
-    promyelocytes = models.FloatField('Количество милеоцитов', default=0.4)
-    neutrophils_myelocytes = models.FloatField('Количество нейтрофилов', default=0.3)
-    neutrphils_metamyelocytes = models.FloatField('Нейтрофилы: Миелоциты', default=0.2)
-
-    def __str__(self):
-        return self.blast_cell
 
 class Deviation(models.Model):
     """"Отклонения"""
