6b41e044f6dfa9ccdb8a4724f6a0e7557b6477ba
roman <33gra@mail.ru>
2021-04-15 21:35:19 +0300

    nir add forms

---
+++ django1/main/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+
+from .models import Patient
+
+
+class PatientForm(forms.ModelForm):
+    class Meta:
+        model = Patient
+        fields = ['surname', 'name', 'patronymic','position_at_work']
+
+
+class UpdatePatientForm(forms.Form):
+    patient_id = forms.IntegerField(required=True)
+    surname = forms.CharField(required=False)
+    name = forms.CharField(required=False)
+    patronymic = forms.CharField(required=False)
+    position_at_work = forms.CharField(required=False)
+
+
+
+class ResultForm(forms.Form):
+    patient_id = forms.IntegerField()
+
+    # blast_cell = forms.FloatField()
+    # promyelocytes = forms.FloatField()
+    # neutrophils_myelocytes = forms.FloatField()
+    # neutrphils_metamyelocytes = forms.FloatField()
+
+    structure_asymmetry = forms.BooleanField(required=False)
+    blue_white_structures = forms.BooleanField(required=False)
+    atypical_pigment_network = forms.BooleanField(required=False)
+    radial_radiance = forms.BooleanField(required=False)
+    points = forms.BooleanField(required=False)
+
+
+class UpdateResultForm(forms.Form):
+    id = forms.IntegerField()
+    patient_id = forms.IntegerField(required=False)
+    # blast_cell = forms.FloatField(required=False)
+    # promyelocytes = forms.FloatField(required=False)
+    # neutrophils_myelocytes = forms.FloatField(required=False)
+    # neutrphils_metamyelocytes = forms.FloatField(required=False)
+    structure_asymmetry = forms.BooleanField(required=True)
+    blue_white_structures = forms.BooleanField(required=True)
+    atypical_pigment_network = forms.BooleanField(required=True)
+    radial_radiance = forms.BooleanField(required=True)
+    points = forms.BooleanField(required=True)
+
+
