2a6a398d3e668a6f07c96cfcb7a79a82def75f3e
roman <33gra@mail.ru>
2021-04-17 16:28:56 +0300

    third version without image

---
+++ django1/main/settings.py
@@ -0,0 +1,15 @@
+NORMAL_MEASURE = {
+    # Количество бластных клеток
+    # "blast_cell_min": 1,
+    # "blast_cell_max": 22,
+    # # Количество милеоцитов
+    # "promyelocytes_min": 1,
+    # "promyelocytes_max": 333,
+    # # Количество нейтрофилов
+    # "neutrophils_myelocytes_min": 0,
+    # "neutrophils_myelocytes_max": 21,
+    # # Нейтрофилы: Миелоциты
+    # "neutrphils_metamyelocytes_min": 0,
+    # "neutrphils_metamyelocytes_max": 2,
+}
+
