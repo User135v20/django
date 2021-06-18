63bb5c3a0f821ee76f332e14af65547ecbb247ec
Gentuyk Valentin <33gva@mail.ru>
2021-06-18 23:11:53 +0300

    delete "diagnosis" table

--- django1/main/admin.py
+++ django1/main/admin.py
@@ -2,12 +2,10 @@ from django.contrib import admin
 from .models import User
 from .models import Result
 from .models import Image
-from  .models import Diagnosis
 
 # Register your models here.
 admin.site.register(User)
 admin.site.register(Result)
 admin.site.register(Image)
-admin.site.register(Diagnosis)
 
 
