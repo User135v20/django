83097070a0210b62e791d9c96b357a1e503491cd
Gentuyk Valentin <33gva@mail.ru>
2021-06-22 23:19:25 +0300

    fix urls https://stackoverflow.com/questions/47661536/django-2-0-path-error-2-0-w001-has-a-route-that-contains-p-begins-wit

--- django1/main/urls.py
+++ django1/main/urls.py
@@ -10,18 +10,18 @@ urlpatterns = [
 
     path('create_result', views.ResultView.create, name='create_result'),
     path('results', views.ResultView.list, name='all_results'),
-    path('result/(?P<pk>[0-9]+)/$', views.ResultView.get, name='result'),
-    path('delete_result/(?P<pk>[0-9]+)/$', views.ResultView.delete, name='delete_result'),
+    path('result/<int:pk>/', views.ResultView.get, name='result'),
+    path('delete_result/<int:pk>/', views.ResultView.delete, name='delete_result'),
     path('update_result', views.ResultView.update, name='update_result'),
 
     path('users', views.UserView.list, name='users'),
     path('new_user', views.UserView.create, name='new_user'),
-    path('delete_user/(?P<pk>[0-9]+)/$', views.UserView.delete, name='delete_user'),
+    path('delete_user/<int:pk>/', views.UserView.delete, name='delete_user'),
     path('update_user', views.UserView.update, name='update_user'),
 
     path('add_image', views.ImageView.add, name='add_image'),
     path('all_images', views.ImageView.list, name='all_images'),
-    path('delete_image/(?P<pk>[0-9]+)/$', views.ImageView.delete, name='delete_image'),
+    path('delete_image/<int:pk>/', views.ImageView.delete, name='delete_image'),
     path('download', views.ImageView.download, name='download'),
     path('download_images', views.ImageView.download_images, name='download_images'),
 ]
